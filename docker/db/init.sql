401a7b6cf537e23a43d7b73c9ed545eceab27443
Dev-Gabriel-Martins <gabrielgmartins100@gmail.com>
2025-05-24 16:46:50 -0300

    build: add docker

---
+++ docker/db/init.sql
@@ -0,0 +1,8 @@
+USE 'contacts-db';
+
+CREATE TABLE IF NOT EXISTS test_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO test_table (name) VALUES ('MariaDB funcionando!');
