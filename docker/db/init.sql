7474c1330c8cf62c843e2706e024b62d398814bb
Dev-Gabriel-Martins <gabrielgmartins100@gmail.com>
2025-05-27 15:11:57 -0300

    feat: add doctrine console script and initialize database schema with users and contacts tables

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,8 +1,16 @@
-USE 'contacts-db';
+CREATE DATABASE IF NOT EXISTS `contacts-db`;
+USE `contacts-db`;
 
-CREATE TABLE IF NOT EXISTS test_table (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    cpf VARCHAR(11) NOT NULL
 );
 
-INSERT INTO test_table (name) VALUES ('MariaDB funcionando!');
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    type BOOLEAN NOT NULL, -- 0 = Telefone, 1 = Email
+    description VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    CONSTRAINT fk_user_contact FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
